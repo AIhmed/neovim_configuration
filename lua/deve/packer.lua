3a928801127ddf25015552f5714b1265defea067
ahmedeve <a.bouliche@m2i-services>
2023-12-31 15:11:18 +0100

    fixed colorscheme transparent issue

--- lua/deve/packer.lua
+++ lua/deve/packer.lua
@@ -6,10 +6,11 @@ return require('packer').startup(function(use)
 
 
     -- color schemes 
-	use 'folke/tokyonight.nvim'
-    --- use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use 'folke/tokyonight.nvim'
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
 	-- install telescope
     use "rebelot/kanagawa.nvim"
+    use { "ellisonleao/gruvbox.nvim" }
 
 
     use {
