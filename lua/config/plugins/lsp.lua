e46ba7aeafd4a64a4b18f7ba13e88ac55273cd49
AIhmed <bouliche.ahmed.2@gmail.com>
2025-05-26 21:17:07 +0100

    fixed volar config

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,153 +1,154 @@
 return {
-	{
-		"neovim/nvim-lspconfig", -- LSP Config
-		config = function()
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-					local opts = { buffer = ev.buf, remap = false }
-					vim.keymap.set("n", "gd", function()
-						vim.lsp.buf.definition()
-					end, opts)
-					vim.keymap.set("n", "K", function()
-						vim.lsp.buf.hover()
-					end, opts)
-					vim.keymap.set("n", "<leader>ws", function()
-						vim.lsp.buf.workspace_symbol()
-					end, opts)
-					vim.keymap.set("n", "<leader>D", function()
-						vim.diagnostic.open_float()
-					end, opts)
-					vim.keymap.set("n", "<leader>Di", function()
-						vim.diagnostic.disable()
-					end, opts)
-					vim.keymap.set("n", "<leader>De", function()
-						vim.diagnostic.enable()
-					end, opts)
-					vim.keymap.set("n", "gn", function()
-						vim.diagnostic.goto_next()
-					end, opts)
-					vim.keymap.set("n", "gp", function()
-						vim.diagnostic.goto_prev()
-					end, opts)
-					vim.keymap.set("n", "<leader>ca", function()
-						vim.lsp.buf.code_action()
-					end, opts)
-					vim.keymap.set("n", "<leader>rf", function()
-						vim.lsp.buf.references()
-					end, opts)
-					vim.keymap.set("n", "<leader>rn", function()
-						vim.lsp.buf.rename()
-					end, opts)
-					vim.keymap.set("i", "<C-h>", function()
-						vim.lsp.buf.signature_help()
-					end, opts)
-				end,
-			})
-			local lspconfig = require("lspconfig")
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- Define common capabilities with LSP enhancements
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+      
+      -- Common on_attach function for all LSP servers
+      local on_attach = function(client, bufnr)
+        vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+        
+        local opts = { buffer = bufnr, remap = false }
+        local keymap = vim.keymap.set
+        
+        -- Key mappings
+        keymap("n", "gd", vim.lsp.buf.definition, opts)
+        keymap("n", "K", vim.lsp.buf.hover, opts)
+        keymap("n", "<leader>ws", vim.lsp.buf.workspace_symbol, opts)
+        keymap("n", "<leader>D", vim.diagnostic.open_float, opts)
+        keymap("n", "<leader>Di", vim.diagnostic.disable, opts)
+        keymap("n", "<leader>De", vim.diagnostic.enable, opts)
+        keymap("n", "gn", vim.diagnostic.goto_next, opts)
+        keymap("n", "gp", vim.diagnostic.goto_prev, opts)
+        keymap("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        keymap("n", "<leader>rf", vim.lsp.buf.references, opts)
+        keymap("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        keymap("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+        
+        -- Format on save if the server supports it
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = vim.api.nvim_create_augroup("LspFormatting", { clear = true }),
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end
+          })
+        end
+      end
 
-			-- Emmet LSP
-			lspconfig.emmet_ls.setup({
-				filetypes = { "html", "css", "javascriptreact", "typescriptreact", "vue", "blade" },
-			})
+      local lspconfig = require("lspconfig")
 
-			-- Intelephense (PHP LSP)
-			lspconfig.intelephense.setup({
-				settings = {
-					intelephense = {
-						stubs = {
-							"bcmath",
-							"bz2",
-							"Core",
-							"curl",
-							"date",
-							"dom",
-							"fileinfo",
-							"filter",
-							"gd",
-							"gettext",
-							"hash",
-							"iconv",
-							"json",
-							"libxml",
-							"mbstring",
-							"openssl",
-							"pcre",
-							"PDO",
-							"pdo_mysql",
-							"Phar",
-							"readline",
-							"Reflection",
-							"session",
-							"SimpleXML",
-							"sockets",
-							"sodium",
-							"standard",
-							"tokenizer",
-							"xml",
-							"xdebug",
-							"zip",
-							"zlib",
-							"wordpress",
-							"woocommerce",
-							"acf",
-							"phpunit",
-						},
-						environment = {
-							phpVersion = "8.1",
-						},
-					},
-				},
-			})
-			lspconfig.ts_ls.setup({
-				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
-				root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
-				single_file_support = true,
-				cmd = { "typescript-language-server", "--stdio" },
-			})
+      -- Emmet LSP (improved config)
+      lspconfig.emmet_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { 
+          "html", "css", "javascriptreact", 
+          "typescriptreact", "vue", "blade",
+          "javascript", "typescript"  -- Added these for better JS/TS support
+        },
+      })
 
-			lspconfig.gopls.setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-				settings = {
-					gopls = {
-						analyses = {
-							unusedparams = true,
-						},
-						staticcheck = true,
-						gofumpt = false,
-					},
-				},
-			})
+      -- PHP (Intelephense)
+      lspconfig.intelephense.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          intelephense = {
+            stubs = {
+              "bcmath", "bz2", "Core", "curl", "date", "dom", "fileinfo",
+              "filter", "gd", "gettext", "hash", "iconv", "json", "libxml",
+              "mbstring", "openssl", "pcre", "PDO", "pdo_mysql", "Phar",
+              "readline", "Reflection", "session", "SimpleXML", "sockets",
+              "sodium", "standard", "tokenizer", "xml", "xdebug", "zip",
+              "zlib", "wordpress", "woocommerce", "acf", "phpunit"
+            },
+            environment = { phpVersion = "8.1" },
+          },
+        },
+      })
 
-			-- Vue.js LSP (Volar)
-			lspconfig.volar.setup({
-				filetypes = { "vue", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-				root_dir = lspconfig.util.root_pattern(
-					"package.json",
-					"vue.config.js",
-					"vite.config.ts",
-					"vite.config.js",
-					".git"
-				),
-				cmd = { "vue-language-server", "--stdio" }, -- Correct way to start Volar
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim", -- Mason for LSP, DAP, linters, and formatters
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim", -- Bridge Mason and LSPconfig
-		dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "gopls", "intelephense", "ts_ls", "volar", "emmet_ls" }, -- Auto-install LSPs
-			})
-		end,
-	},
+      -- TypeScript (using tsserver with improved config)
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { 
+          "javascript", "typescript", 
+          "javascriptreact", "typescriptreact",
+          "vue"  -- Important for Vue SFC support
+        },
+        root_dir = lspconfig.util.root_pattern(
+          "package.json", "tsconfig.json", "jsconfig.json", ".git"
+        ),
+        init_options = {
+          preferences = {
+            importModuleSpecifierPreference = "relative",
+            jsxAttributeCompletionStyle = "auto"
+          }
+        }
+      })
+
+      -- Go (gopls)
+      lspconfig.gopls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        settings = {
+          gopls = {
+            analyses = { unusedparams = true },
+            staticcheck = true,
+            gofumpt = false,
+          },
+        },
+      })
+
+      -- Vue.js (using volar instead of vue_ls)
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+        filetypes = { "vue", "javascript", "typescript" },
+        init_options = {
+          vue = {
+            hybridMode = false,  -- Set to true if using Vue 2
+          },
+          typescript = {
+            tsdk = require('lspconfig.util').get_typescript_server_path()
+          }
+        },
+      })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { 
+          "gopls", 
+          "intelephense", 
+          "tsserver",  -- Changed from ts_ls to tsserver
+          "emmet_ls",
+          "vue_ls"      -- Added volar for Vue
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
 }
